ef53245c4c66f3fed6e4e37dd5af940f4d6c380c
Snehasish Das <Snehasish.Das@247-inc.com>
2023-02-08 13:04:59 +0530

    updating tcm url

--- api/src/config/init.php
+++ api/src/config/init.php
@@ -1,5 +1,6 @@
 <?php
-    define('BASE_URL','http://localhost/attest');
+    define('BASE_URL','http://localhost/attest'); //local base url
+    //define('BASE_URL','http://dev-cache01.scs.dev.east.cloud.247-inc.net/attest'); //247 base url
     define('JENKINS_URL','https://cicd.cloud.247-inc.net');
     define('JENKINS_USER','snehasish.das@247.ai');
     define('JENKINS_PWD','114abfad369c314f5ad78546270daf2ea6');
